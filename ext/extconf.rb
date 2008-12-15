ea02a110b623d278395d693a67b117084fb70959
Andre Arko <andre@arko.net>
2008-12-15 15:51:19 -0800

    Allow the location of lib-oniguruma to be passed in
    For example, to install via ports on Mac OS X 10.5:
    sudo port install oniguruma5
    sudo gem install oniguruma -- --with-onig-dir=/opt/local

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,9 @@
 require 'mkmf'
-have_library("onig")
-$CFLAGS='-Wall'
-create_makefile( "oregexp" )
+dir_config("onig")
+if have_library("onig")
+  $CFLAGS='-Wall'
+  create_makefile( "oregexp" )
+else
+  puts "Cannot find lib-oniguruma. Set location with e.g. --with-onig-dir=/opt/local"
+  exit 1
+end
